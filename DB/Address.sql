16968b94054cb606fc4961abf7b466a8de8d169c
Kristiyan Manov <93081298+KristiyanManow@users.noreply.github.com>
2022-05-09 15:18:05 +0300

    a
    aa

---
+++ DB/Address.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [AddresLine] NCHAR(50) NULL, 
+    [District] NCHAR(50) NULL, 
+    [Municipality] NCHAR(50) NULL, 
+    [City] NCHAR(50) NULL
+)
